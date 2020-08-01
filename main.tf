6f721aa35f5bf667f6f770f6613fd4f2f34e5f8a
eiryu <eiryu.co@gmail.com>
2020-08-02 04:09:33 +0900

    add Terraform Cloud setting

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
+variable "terraform_organization" {}
+variable "terraform_workspace_name" {}
+
 terraform {
   required_version = "~> 0.12"
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = var.terraform_organization
+
+    workspaces {
+      name = var.terraform_workspace_name
+    }
+  }
 }
 
 provider "github" {
